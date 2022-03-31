f9e018d92a9a311c6a23525c23f393dcbe17fb1f
SteliosKpz <stelioskpz@gmail.com>
2022-03-31 16:19:21 +0300

    optimized renderer for weaker devices

--- src/app/scene/scene.component.ts
+++ src/app/scene/scene.component.ts
@@ -16,24 +16,26 @@ import * as THREE from 'three';
 })
 export class SceneComponent implements AfterViewInit {
   constructor() {}
-  @Input() pColor=0xF7CE68
+ /**
+  * Initializing the variables for the scene
+  */
   lastScrollTop = 0;
   @ViewChild('canvas')
   private canvasRef: ElementRef;
   rotationSpeedX: number = 0.05;
   rotationSpeedY: number = 0.002;
   textureLoader = new THREE.TextureLoader();
-  planet1Texture = this.textureLoader.load('assets/Planet1.png');
+  planet1Texture: THREE.Texture = this.textureLoader.load('assets/Planet1.png');
   planet2Texture = this.textureLoader.load('assets/NormalMap.jpg');
-  geometry = new THREE.IcosahedronGeometry(80, 7);
+  geometry = new THREE.IcosahedronBufferGeometry(80, 6);
   material = new THREE.MeshPhongMaterial({
-    emissive: this.pColor,
+    emissive: 0xf7ce68,
     emissiveIntensity: 0.7,
     shininess: 0,
     normalMap: this.planet1Texture,
   });
   material2 = new THREE.MeshPhongMaterial({
-    emissive: 0xFBAB7E,
+    emissive: 0xfbab7e,
     emissiveIntensity: 0.7,
     shininess: 50,
     normalMap: this.planet2Texture,
@@ -43,7 +45,7 @@ export class SceneComponent implements AfterViewInit {
   renderer: THREE.WebGLRenderer;
   scene: THREE.Scene;
   private camera = new THREE.PerspectiveCamera(
-    100,
+    90,
     window.innerWidth / window.innerHeight,
     0.1,
     10000
@@ -51,7 +53,9 @@ export class SceneComponent implements AfterViewInit {
   private get canvas(): HTMLCanvasElement {
     return this.canvasRef.nativeElement;
   }
-
+ /**
+  * AfterViewInit start rendering
+  */
   ngAfterViewInit(): void {
     this.createScene();
     this.startRender();
@@ -61,9 +65,10 @@ export class SceneComponent implements AfterViewInit {
       this.renderer.render(this.scene, this.camera);
     });
   }
-changepColor(){
 
-}
+/**
+ * Adding the created objects,lighting to the scene
+ */
   private createScene() {
     var light = new THREE.HemisphereLight(0xffffff, 0x0c056d, 0.4);
     this.scene = new THREE.Scene();
@@ -71,17 +76,25 @@ changepColor(){
       'assets/blurry-gradient-haikei (1).svg'
     );
     this.scene.add(light);
-    this.shape.position.set(50, 10, 30);
-    this.shape2.position.set(225, 3, -150);
+    this.shape.position.set(50, 10, 10);
+    this.shape2.position.set(225, 3, -190);
     this.camera.position.set(60, 0, 200);
     this.scene.add(this.shape);
     this.scene.add(this.shape2);
     this.shape.position.y = 1;
   }
+
+  /**
+   * Start the render,(optimized by locking framerate to 30fps and disabling antialiasing when pixel ratio is >1 since there is no apparent visual gain while performance tanks)
+   */
+
   startRender() {
+    console.log(window.devicePixelRatio)
+    let AA = window.devicePixelRatio > 1 ? false : true;
     this.renderer = new THREE.WebGLRenderer({
+      powerPreference: 'high-performance',
       canvas: this.canvas,
-      antialias: true,
+      antialias: AA,
     });
     this.renderer.setPixelRatio(window.devicePixelRatio);
     this.renderer.setSize(this.canvas.clientWidth, this.canvas.clientHeight);
@@ -89,9 +102,14 @@ changepColor(){
     component.renderer.render(component.scene, component.camera);
     render();
     function render() {
-      requestAnimationFrame(render);
-      component.animateCube();
-      component.renderer.render(component.scene, component.camera);
+      setTimeout(() => {
+        console.log(component.renderer.info.render.frame);
+      }, 1000);
+      setTimeout(() => {
+        requestAnimationFrame(render);
+        component.animateSphere();
+        component.renderer.render(component.scene, component.camera);
+      }, 1000 / 15);
     }
     window.addEventListener('scroll', () => {
       this.shape.rotation.y += this.rotationSpeedY * 2;
@@ -99,10 +117,10 @@ changepColor(){
     });
   }
 
-  animateCube() {
+  animateSphere() {
     //this.shape.rotation.x+=this.rotationSpeedX
     this.shape.rotation.y += this.rotationSpeedY;
     // this.shape2.rotation.x+=this.rotationSpeedX
-    this.shape2.rotation.y += 0.001;
+    this.shape2.rotation.y += 0.002;
   }
 }
